aa8b45853ef6226fde655dab56c404bab83915f1
Owcollier <owcollier@gmail.com>
2018-02-01 14:45:57 -0500

    still debugging qsort

--- qsort.js
+++ qsort.js
@@ -8,41 +8,44 @@
 // When i & j stops, swap elements at i and j
 // When i > j, swap i with pivot
 
-function quickSort(array, start = 0, end = array.length){
+function quickSort(array, start = 0, end = array.length - 1){
+
   if (start >= end) {
     return array;
   }
+  console.log('start:', start);
+  console.log('end:', end);
   // console.log('here');
   const middle = partition(array, start, end);
+  console.log('middle:', middle);
   // console.log('here?');
   array = quickSort(array, start, middle);
-  // console.log('array after left recursion', array);
+  console.log(array);
+  
   array = quickSort(array, middle + 1, end);
-  // console.log('array after right recursion', array);
+  
   return array;
 }
 
 function partition(array, start, end){
-  let pivot = array[Math.floor(end/2)];
-  let j = end - 1;
+  let pivot = Math.floor(end/2);
+  let j = end;
   let i = start;
   while(i < j){
-    if (array[i] >= pivot && array[j] <= pivot){
+    if (array[i] >= array[pivot] && array[j] <= array[pivot]){
       swap(array, i, j);
       j--;
       i++;
     }
-    else if (array[i] <= pivot) {
+    else if (array[i] <= array[pivot]) {
       i++;
     }
-    else if (array[j] >= pivot){
+    else if (array[j] >= array[pivot]){
       j--;
     }
   }
-  console.log('pivot', pivot);
   swap(array, i, pivot);
-  console.log('i', i);
-  return i;
+  return pivot;
 }
 
 function swap(array, i, j) {
